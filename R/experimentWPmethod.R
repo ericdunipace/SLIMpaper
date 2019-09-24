56cc74645ece1a35d7a5435ff543a0459e75308c
ericdunipace <edunipace@g.harvard.edu>
2019-09-24 18:05:14 -0400

    increasing estimation time to 6 hours for simulatead annealing

--- R/experimentWPmethod.R
+++ R/experimentWPmethod.R
@@ -21,7 +21,7 @@ experimentWPMethod <- function(target, hyperparameters, conditions, w2=FALSE) {
   if(is.null(not.only.timing)) not.only.timing <- FALSE
 
   sa_seq <- sort(unique(c(2,5,floor(seq(ceiling(p/5),p,floor(p/5))))))
-  sa_max_time <- 10800
+  sa_max_time <- 21600
   sa_prop <- "random"
   # FSAiter <- 10*1:ceiling(p/2)
   # RSAiter <- if( p %% 2) { rev(FSAiter)[-1] } else { rev(FSAiter) }
