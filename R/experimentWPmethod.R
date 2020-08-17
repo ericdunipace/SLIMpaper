0eda4a733ff771679915e74abf2dc4f77f1431f6
ericdunipace <edunipace@g.harvard.edu>
2020-08-17 23:05:10 +0200

    bug fix

--- R/experimentWPmethod.R
+++ R/experimentWPmethod.R
@@ -367,7 +367,7 @@ experimentWPMethod <- function(target, hyperparameters, conditions) {
                        penalty.factor=proj_penalty_fact, nlambda = n.lambda,
                        lambda.min.ratio = lambda.min.ratio, infimum.maxit=1,
                        maxit=1e5, alpha = 0.99, gamma = 1.1,
-                       transport.method = transport.method, maxit = 1e5,
+                       transport.method = transport.method,
                        display.progress=TRUE, method = "projection")
 
     cat(" SW, ",date(),"\n")
